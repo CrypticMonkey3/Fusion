a8329044a0a9b10655a38f9b99996562813b4391
CrypticMonkey3 <78447236+CrypticMonkey3@users.noreply.github.com>
2023-12-16 20:09:37 +0000

    Extracting instrument data: notes, from MIDI file and storing into lists

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 
 ## Dataset
  https://www.kaggle.com/datasets/zakarii/lofi-hip-hop-midi/
+ 
+## Guides being used
+https://medium.com/mlearning-ai/how-to-generate-music-using-machine-learning-72360ba4a085
+
+https://thehackerstuff.com/getting-started-with-ai-music-generation-exploring-the-possibilities/
--- main.py
+++ main.py
@@ -8,21 +8,55 @@ class Fusion:
     def __init__(self):
         self.__running = True
 
-    # The preprocessing code was taken from this page:
-    # https://thehackerstuff.com/getting-started-with-ai-music-generation-exploring-the-possibilities/
     def __preprocess_midi(self, file_dir: str):
         """
 
         :param str file_dir: The directory of the file we want to use.
         :return:
         """
+        pitches, durations, velocities = [], [], []
+
         midi_data = converter.parse(file_dir)  # loads MIDI file and makes it a Music21 stream object
 
-        pitches, durations, velocities = [], [], []
-        for note in midi_data.notes:
-            pitches.append(note.pitch.midi)
-            durations.append(note.duration.quarterLength)
-            velocities.append(note.velocity)
+        notes_to_parse = midi_data.flatten().notes  # assume file has notes in a flat structure
+        instruments = instrument.partitionByInstrument(midi_data)
+        print(instruments)
+        if instruments is not None:  # if the file has instrumental parts
+            # creates a RecursiveIterator object from it.
+            notes_to_parse = instruments.recurse()
+
+            print(instruments.recurse())
+
+        print(notes_to_parse)
+        for element in notes_to_parse:  # goes through each note in the midi file
+            match type(element):
+                case note.Note:
+                    pitches.append(str(element.pitch))
+                    durations.append(str(element.duration.quarterLength))
+
+                case chord.Chord:
+                    print(".".join(str(_) for _ in element.normalOrder))
+
+        # extract certain attributes from midi data
+        # pitches, durations, velocities = [], [], []
+        # for instr_note in midi_data.flat.notes:
+        #     pitches.append(instr_note.pitch.midi)
+        #     durations.append(instr_note.duration.quarterLength)
+        #     velocities.append(instr_note.velocity)
+        #     print("1")
+        #
+        # print(pitches)
+        # print(durations)
+        # print(velocities)
+        #
+        # # convert pitch, duration, and velocity to numpy arrays: a more efficient data structure for numerical operation
+        # pitches = np.array(pitches)
+        # durations = np.array(durations)
+        # velocities = np.array(velocities)
+        #
+        # print(pitches)
+        # print(durations)
+        # print(velocities)
 
     def __process(self, file_dir: str) -> None:
         """
@@ -41,17 +75,5 @@ class Fusion:
             self.__process(f"Music-Dataset/{os.listdir('Music-Dataset')[file_index]}")
 
 
-x = converter.parse("Music-Dataset/Cymatics - Lofi MIDI 1 - C Maj.mid")
-print(x)
-x = x.makeMeasures()  # splits original music into measures
-print(x)
-x = x.flat  # combines the measures to a single stream
-print(x)
-x = x.getElementsByClass("Note")  # selects all the notes from the stream
-print(x)
-
-
 f = Fusion()
 f.run()
-
-
